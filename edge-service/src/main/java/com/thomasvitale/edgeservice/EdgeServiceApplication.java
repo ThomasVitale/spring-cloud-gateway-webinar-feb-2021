93a9fd1b585dbba73352900456fd658db6de6e9f
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-02-25 16:50:37 +0100

    Route example

--- edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
@@ -1,21 +1,12 @@
 package com.thomasvitale.edgeservice;
 
-import reactor.core.publisher.Mono;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdgeServiceApplication.class, args);
-	}
-
-	@Bean
-	public KeyResolver keyResolver() {
-		return exchange -> Mono.just("USER");
 	}
 }
